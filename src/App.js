4a6921416effa2dbb2dacc98271c5706ef356a47
Jerrick Backous <77847817+JerrickBackous@users.noreply.github.com>
2023-08-03 17:27:58 -0500

    catch data from inputs

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="app">
       <Logo />
       <Form />
-      <PackingList />
+      <PackingList items = { initialItems } />
       <Stats />
     </div>
   );
@@ -24,15 +24,27 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select
+      value={quantity}
+      onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -50,11 +62,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
